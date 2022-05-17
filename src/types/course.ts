303c299a5f45b7a8ecdd788b8ff5849a11ef7976
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-05-17 21:49:03 +0700

    test api

---
+++ src/types/course.ts
@@ -0,0 +1,10 @@
+export interface IGetCourse {
+  page: number;
+  limit: number;
+  sort?: string;
+  name?: string;
+  category?: string;
+  tags?: string;
+  price?: string;
+  publish?: boolean;
+}
