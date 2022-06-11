2a47e200622c374b6bfab845d8b6013bc0cf63d5
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-06-11 21:07:13 +0700

    add get courses

--- src/pages/AdminPage/CoursesManage/CourseList.tsx
+++ src/pages/AdminPage/CoursesManage/CourseList.tsx
@@ -1,6 +1,10 @@
 import { GridColDef } from "@mui/x-data-grid";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import courseApi from "src/apis/courseApi";
 import Table from "src/components/Table/Table";
+import useTypingDebounce from "src/hooks/useTypingDebounce";
+import { ICourse } from "src/types";
+import { getHeaderColumns, getNewHeaderColumn } from "src/utils/table";
 
 const columsHeader: GridColDef[] = [
   {
@@ -18,37 +22,83 @@ const columsHeader: GridColDef[] = [
   },
   {
     field: "status",
-    headerName: "Trang thái",
+    headerName: "Trạng thái",
     width: 120,
     align: "center",
     headerAlign: "center",
   },
   {
-    field: "role",
-    headerName: "Chức vụ",
-    width: 120,
+    field: "name",
+    headerName: "Tên khoá học",
+    width: 500,
   },
   {
-    field: "fullName",
-    headerName: "Họ và tên",
+    field: "author",
+    headerName: "Tác giả",
     width: 150,
   },
   {
-    field: "gender",
-    headerName: "Giới tính",
-    width: 120,
+    field: "currentPrice",
+    headerName: "Giá hiện tại",
+    width: 100,
   },
   {
-    field: "email",
-    headerName: "Địa chỉ email",
-    width: 200,
+    field: "originalPrice",
+    headerName: "Giá gốc",
+    width: 100,
   },
-  { field: "phone", headerName: "Số điện thoại", width: 200 },
 ];
 
 const CourseList = () => {
   document.title = "Quản lý khoá học";
   const [loading, setLoading] = useState<boolean>(false);
+  const [courses, setCourses] = useState<ICourse[]>([]);
+  const [isActive, setIsActive] = useState<boolean>(true);
+
+  //pagination
+  const [total, setTotal] = useState<number>(0);
+  const [pageSize, setPageSize] = useState<number>(5);
+  const [page, setPage] = useState<number>(1);
+
+  //debounce
+  const [value, setValue] = useState<string>();
+  const debouncedValue = useTypingDebounce(value);
+  const [name, setName] = useState<string>();
+
+  useEffect(() => {
+    getCourses();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, pageSize, name]);
+
+  const getCourses = async () => {
+    setLoading(true);
+    const params = { page, limit: pageSize, active: isActive, name };
+
+    try {
+      const response = await courseApi.getCourses(params);
+      const { courses, total }: any = response;
+      console.log("course", response);
+      if (courses.length > 0) {
+        const keys = getHeaderColumns(courses[0]);
+        const data = getNewHeaderColumn(courses, keys, page, pageSize);
+
+        const courseData = data.map((data, index) => {
+          return {
+            ...data,
+            author: courses[index].author.fullName,
+          };
+        });
+        setCourses(courseData);
+      } else {
+        setCourses(courses);
+      }
+      setLoading(false);
+      setTotal(total);
+    } catch (error) {
+      setLoading(false);
+      console.log("lỗi rồi", { error });
+    }
+  };
 
   return (
     <Table
@@ -56,8 +106,11 @@ const CourseList = () => {
       isLoading={loading}
       title="Danh sách thông tin người dùng"
       columnsData={columsHeader}
-      // rowsData={users}
-      // total={total}
+      getRowId={(row) => row._id}
+      onPage={(page) => setPage(Number(page))}
+      onPageSize={(pageSize) => setPageSize(Number(pageSize))}
+      total={total}
+      rowsData={courses}
       // handleAddItem={handleCreate}
       // onDeleteItem={handleDelete}
       // onModifyItem={handleModifyItem}
