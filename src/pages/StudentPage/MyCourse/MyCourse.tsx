5f1967c69784f848193928b29a0eb00a51aa2d54
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-06-24 14:28:24 +0700

    fix progress my course item detail

--- src/pages/StudentPage/MyCourse/MyCourse.tsx
+++ src/pages/StudentPage/MyCourse/MyCourse.tsx
@@ -13,28 +13,30 @@ export default function MyCourse() {
 
   const [isUpdate, setIsUpdate] = useState<boolean>(false);
 
-  //pagination
-  const [total, setTotal] = useState<number>(0);
-  const [pageSize, setPageSize] = useState<number>(5);
-  const [page, setPage] = useState<number>(1);
+  // //pagination
+  // const [total, setTotal] = useState<number>(0);
+  // const [pageSize, setPageSize] = useState<number>(5);
+  // const [page, setPage] = useState<number>(1);
 
   useEffect(() => {
     if (!isUpdate) {
       getMyCourse();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pageSize, page, isUpdate]);
+  }, [isUpdate]);
 
   const getMyCourse = async () => {
-    const params = { limit: pageSize, page };
+    // const params = { limit: pageSize, page };
     try {
-      const response = await myCourseApi.getMyCourse(params);
+      // const response = await myCourseApi.getMyCourse(params);
+      const response = await myCourseApi.getMyCourse();
       // console.log("ádadas", response);
-      const { myCourses, total }: any = response;
+      // const { myCourses, total }: any = response;
+      const { myCourses }: any = response;
       // console.log("myCourses", myCourses);
       setCourses(myCourses);
 
-      setTotal(total);
+      // setTotal(total);
     } catch (error) {
       console.log("lỗi rồi", { error });
     }
