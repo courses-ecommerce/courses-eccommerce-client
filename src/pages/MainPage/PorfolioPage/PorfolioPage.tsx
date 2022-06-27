db224bc30329131ebe2d754df1e3c07096c61e21
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-06-27 14:13:45 +0700

    porfolio page

--- src/pages/MainPage/PorfolioPage/PorfolioPage.tsx
+++ src/pages/MainPage/PorfolioPage/PorfolioPage.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import teacherApi from "src/apis/teacherApi";
 import "./PorfolioPage.scss";
 
 const PorfolioPage = () => {
   document.title = "Thông tin chi tiết giảng viên";
   const navigate = useNavigate();
   const { id } = useParams();
+
+  useEffect(() => {
+    getInfoTeacher();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  const getInfoTeacher = async () => {
+    try {
+      const response = await teacherApi.getTeacherInfoById(id);
+      console.log("response", response);
+    } catch (error) {
+      console.log("lỗi rỗi", { error });
+    }
+  };
 
   return (
     <>
