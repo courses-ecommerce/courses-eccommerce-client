48edcbdb229857c891ef09997db1bf52ebb22c2a
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-06-28 00:21:19 +0700

    add suggest course

--- src/apis/courseApi.ts
+++ src/apis/courseApi.ts
@@ -12,9 +12,9 @@ const courseApi = {
     const url = COURSE_API + "/hot";
     return axiosClient.get(url, { params });
   },
-  getCoursesSuggest: (limit: Object) => {
+  getCoursesSuggest: (params?: any) => {
     const url = COURSE_API + "/suggest";
-    return axiosClient.get(url);
+    return axiosClient.get(url, { params });
   },
   getCoursesRelated: (id?: string, params?: any) => {
     const url = COURSE_API + "/" + id + "/related";
