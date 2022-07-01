8e43723ba20ca39cb9602441d8e01b725624bb27
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-07-01 22:21:32 +0700

    fix navs

--- src/components/NavigationHeader/NavigationHeader.tsx
+++ src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { linkUserProfile } from "src/data/sidebar";
+import AccountPopover from "../AccountPopover/AccountPopover";
 import "./NavigationHeader.scss";
 
 export default function NavigationHeader() {
   const navigate = useNavigate();
   return (
     <div className="navs">
-      <span onClick={() => navigate(-1)}>Quay lại trang trước</span>
-      <Link to="/"></Link>
+      <div className="navLinks">
+        <span onClick={() => navigate(-1)}>Quay lại trang trước</span>
+        <Link to="/"></Link>
+      </div>
+      <AccountPopover routes={linkUserProfile} />
     </div>
   );
 }
