434f590d62be4ea6c29a84559e669cef4fc252e8
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-07-03 00:55:37 +0700

    fix icon link cart

--- src/layouts/dashboard/DashBoardHeader/DashboardHeader.tsx
+++ src/layouts/dashboard/DashBoardHeader/DashboardHeader.tsx
@@ -7,7 +7,11 @@ import "./DashboardHeader.scss";
 const DashboardHeader = () => {
   return (
     <div className="dashboard-header">
-      <div className="toggle">{/* <Icon icon="bars" size={20} /> */}</div>
+      {/* <div className="toggle">
+        <Icon icon="bars" size={20} />
+      </div> */}
+      <div className="logo"></div>
+
       <div className="content">
         <Notification type="notify" />
         {/* <Notification type="message" /> */}
