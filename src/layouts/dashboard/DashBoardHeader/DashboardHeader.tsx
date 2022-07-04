a11f866c25dfd02a35d95c9220ce9a1c480ff6f2
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-07-04 15:25:57 +0700

    fix code

--- src/layouts/dashboard/DashBoardHeader/DashboardHeader.tsx
+++ src/layouts/dashboard/DashBoardHeader/DashboardHeader.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import AccountPopover from "src/components/AccountPopover/AccountPopover";
 // import CartIcon from "src/components/CartIcon/CartIcon";
 import Notification from "src/components/Notification/Notification";
@@ -5,12 +6,14 @@ import { linkUserProfile } from "src/data/sidebar";
 import "./DashboardHeader.scss";
 
 const DashboardHeader = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="dashboard-header">
       {/* <div className="toggle">
         <Icon icon="bars" size={20} />
       </div> */}
-      <div className="logo"></div>
+      <div className="logo" onClick={() => navigate("/")}></div>
 
       <div className="content">
         <Notification type="notify" />
