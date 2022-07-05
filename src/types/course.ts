0b9122f51811d9a3603767ab9174757704c8103a
Tuyen <9a5.tuyen@gmail.com>
2022-07-05 16:18:30 +0700

    Update status lowercase && show status course

--- src/types/course.ts
+++ src/types/course.ts
@@ -9,3 +9,9 @@ export interface IGetCourse {
   publish?: boolean;
   author?: string;
 }
+
+export enum ICourseStatues {
+  draft = "Bản nháp",
+  pending = "Đang chờ duyệt",
+  approved = "Đã duyệt",
+}
