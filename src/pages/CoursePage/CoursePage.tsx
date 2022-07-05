75941fcb7a8e01e391073fa91f8d922b2fc2e031
esp-NguyenTheLuan <luan.nguyen@eastplayers.io>
2022-07-05 14:46:37 +0700

    fix search

--- src/pages/CoursePage/CoursePage.tsx
+++ src/pages/CoursePage/CoursePage.tsx
@@ -9,7 +9,7 @@ import Pagination from "src/components/Pagination/Pagination";
 import { priceRangeTypes, sortTypes } from "src/data";
 import useTypingDebounce from "src/hooks/useTypingDebounce";
 import { selectAuthorization } from "src/reducers/authSlice";
-import { ICourse } from "src/types";
+import { ICourse, SearchKeyProps } from "src/types";
 import { numberRound } from "src/utils";
 import CourseContainer from "./CourseContainer/CourseContainer";
 import "./CoursePage.scss";
@@ -18,6 +18,8 @@ const CoursePage = () => {
   const { isRole } = useSelector(selectAuthorization);
 
   const [courses, setCourses] = useState<ICourse[]>([]);
+  const [searchKey, setSearchKey] = useState<SearchKeyProps>();
+  const [totalCourse, setTotalCourse] = useState<number>();
   const [page, setPage] = useState(1);
   const [total, setTotal] = useState<number>();
   const [limit, setLimit] = useState<number>(8);
@@ -54,8 +56,6 @@ const CoursePage = () => {
 
   useEffect(() => {
     getCourses();
-    // console.log("sort", sort, "category", category);
-    // console.log("giá là", price === 0 ? { min: 0 } : price);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [limit, page, sort, category, name, price]);
@@ -92,10 +92,14 @@ const CoursePage = () => {
 
     try {
       const response = await courseApi.getCourses(params);
-      const { courses, total }: any = response;
-      console.log("data", response);
+      const { courses, searchKey, total }: any = response;
+      // console.log("data", response);
       // console.log("courses", courses);
+      // console.log("searchkey", searchKey);
+
       setCourses(courses);
+      setSearchKey(searchKey);
+      setTotalCourse(total);
       setTotal(numberRound(total / limit));
     } catch (error) {
       console.log("lỗi rồi", { error });
@@ -119,7 +123,7 @@ const CoursePage = () => {
     try {
       const response = await courseApi.getCoursesSuggest(params);
       const { courses, total }: any = response;
-      console.log("courses hot", courses);
+      // console.log("courses hot", courses);
       setCoursesSuggest(courses);
       setTotalSuggest(numberRound(total / total));
     } catch (error) {
@@ -163,7 +167,10 @@ const CoursePage = () => {
                 hideErrorMessage={true}
                 defaultValue={category}
                 list={categoryList}
-                onChange={(e) => setCategory(e.target.value)}
+                onChange={(e) => {
+                  setPage(1);
+                  setCategory(e.target.value);
+                }}
               />
             </Box>
           )}
@@ -173,7 +180,10 @@ const CoursePage = () => {
               hideErrorMessage={true}
               defaultValue={sort}
               list={sortTypes}
-              onChange={(e) => setSort(e.target.value)}
+              onChange={(e) => {
+                setPage(1);
+                setSort(e.target.value);
+              }}
             />
           </Box>
           <Box sx={{ width: 200 }}>
@@ -181,7 +191,10 @@ const CoursePage = () => {
               hideErrorMessage={true}
               defaultValue={price}
               list={priceRangeTypes}
-              onChange={(e) => setPrice(e.target.value)}
+              onChange={(e) => {
+                setPage(1);
+                setPrice(e.target.value);
+              }}
             />
           </Box>
         </div>
@@ -189,10 +202,21 @@ const CoursePage = () => {
 
       {/* what for u using to search?? */}
       <div className="keyword-to-search">
-        {name && (
-          <span className="title">
-            Đang search với từ khoá <span className="key-search">{name}</span>
-          </span>
+        {searchKey?.suggestion && searchKey.original !== "" ? (
+          <>
+            <span className="key-search">
+              Đang search với từ khoá <b>{searchKey.original}</b>
+            </span>
+            <span>
+              Hiển thị kết quả cho từ <b>{searchKey.suggestion}</b>
+            </span>
+          </>
+        ) : (
+          totalCourse && (
+            <span>
+              Có <b>{totalCourse}</b> kết quả hiển thị
+            </span>
+          )
         )}
       </div>
 
@@ -209,7 +233,9 @@ const CoursePage = () => {
           <Pagination
             pageActive={page}
             total={total}
-            onChangeValue={(value: any) => setPage(value)}
+            onChangeValue={(value: any) => {
+              setPage(value);
+            }}
           />
         </Box>
 
