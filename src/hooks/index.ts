a374c2443347e1973db4c4828f08b387aa7aa7ea
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2022-11-27 21:48:40 +0700

    fix spelling name coppy -> copy

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,5 @@
 export * from "./useClickOutSide";
 export * from "./useTypingDebounce";
-export * from "./useCoppyToClipBoard";
+export * from "./useCopyToClipBoard";
 export * from "./useMediaQuery";
 export * from "./useResponsive";
