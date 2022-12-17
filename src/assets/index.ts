b14132028d26698c512a1b4a521472d906d7f2ad
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2022-12-17 20:20:18 +0700

    update index asset images file

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,10 +1,3 @@
-// images thumbnails
-import thumb1 from "../assets/images/thumb1.jpg";
-import thumb2 from "../assets/images/thumb2.jpg";
-import thumb3 from "../assets/images/thumb3.jpg";
-import thumb4 from "../assets/images/thumb4.jpg";
-import thumb5 from "../assets/images/thumb5.jpg";
-//icon
 import downloadIMG from "../assets/images/download.png";
 import defaultIMG from "../assets/images/image-default.gif";
 import avatarNone from "../assets/images/avt-none.png";
@@ -13,6 +6,5 @@ import upload from "../assets/images/upload.png";
 // hashtag
 import quotes from "../assets/images/quote.svg";
 
-export { thumb1, thumb2, thumb3, thumb4, thumb5 };
 export { downloadIMG, defaultIMG, linearIMG, avatarNone, upload };
 export { quotes };
