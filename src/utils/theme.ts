291fde7b9257866d039f2ce1316390f424959d56
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2023-01-08 23:30:43 +0700

    add theme config

---
+++ src/utils/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  spacing: 1,
+  shape: { borderRadius: 4 },
+  // typography: {
+  //   fontFamily: "fantasy",
+  // },
+  //   palette: {
+  //     mode: "light",
+  //   },
+});
+
+export default theme;
