4f3ce42b1b091f1527d4ebf4ba367c87595e3d97
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2023-02-11 20:56:44 +0700

    Refactor NavigationHeader

--- src/components/NavigationHeader/NavigationHeader.tsx
+++ src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,26 +1,59 @@
+import { Box } from "@mui/system";
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { linkUserProfile } from "src/data";
 import { selectAuthorization } from "src/reducers";
 import AccountPopover from "../AccountPopover";
+import BoxContent from "../BoxContent";
 import CartIcon from "../CartIcon";
-import "./NavigationHeader.scss";
+import TextContent from "../TextContent";
 
-export default function NavigationHeader() {
+const HeaderNavigationStyle: React.CSSProperties = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  borderRadius: 0,
+  padding: "0 24px",
+  background: "white",
+
+  position: "sticky",
+  top: 0,
+  left: 0,
+
+  zIndex: 3,
+};
+
+interface NavigationHeaderProps {
+  title?: string;
+  userAction?: boolean;
+}
+
+export default function NavigationHeader(props: NavigationHeaderProps) {
+  const { title, userAction = true } = props;
+
   const navigate = useNavigate();
   const { isRole } = useSelector(selectAuthorization);
 
   return (
-    <div className="navs">
-      <div className="navLinks">
-        <span onClick={() => navigate(-1)}>Quay lại trang trước</span>
-        {/* <Link to="/"></Link> */}
-      </div>
-      <div className="extraLinks">
-        {isRole === "student" && <CartIcon />}
-        <AccountPopover routes={linkUserProfile} />
-      </div>
-    </div>
+    <BoxContent.BoxShadow shadow="type2" style={HeaderNavigationStyle}>
+      <Box flex={1} display="flex" gap={20} padding="12px 0">
+        <span onClick={() => navigate(-1)}>
+          <TextContent.NormalText
+            content="Quay lại trang trước"
+            style={{ cursor: "pointer" }}
+          />
+        </span>
+        {title && <TextContent.NormalText content={title} />}
+      </Box>
+      {userAction && (
+        <Box className="user-actions" display="flex" alignItems="center">
+          {isRole === "student" && <CartIcon />}
+          <AccountPopover routes={linkUserProfile} />
+        </Box>
+      )}
+    </BoxContent.BoxShadow>
   );
 }
