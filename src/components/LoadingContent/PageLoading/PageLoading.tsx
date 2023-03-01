9dfa1bc97df224a96b13b1b8fe26990576ad98b0
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2023-03-01 20:44:42 +0700

    update page loading

--- src/components/LoadingContent/PageLoading/PageLoading.tsx
+++ src/components/LoadingContent/PageLoading/PageLoading.tsx
@@ -1,18 +1,7 @@
-import { Backdrop } from "@mui/material";
-import { RingLoader } from "react-spinners";
+import "./PageLoading.scss";
 
 const PageLoading = () => {
-  return (
-    <Backdrop
-      open
-      sx={{
-        backgroundColor: "rgba(248, 248, 248, 0.5)",
-        zIndex: 99999,
-      }}
-    >
-      <RingLoader loading color="#7423c4" size={100} />
-    </Backdrop>
-  );
+  return <span className="loader"></span>;
 };
 
 export default PageLoading;
