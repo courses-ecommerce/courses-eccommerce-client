da87999b58a139c0d919d90dbe6f263985bd6319
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2023-08-06 09:57:53 +0700

    update env

--- src/data/link.ts
+++ src/data/link.ts
@@ -1,1 +1,2 @@
-export const LINK_DOMAIN = "https://courses-ecommerce-apis.onrender.com";
+// export const LINK_DOMAIN = "https://courses-ecommerce-apis.onrender.com";
+export const LINK_DOMAIN = "https://api.ce.hnam.id.vn";
