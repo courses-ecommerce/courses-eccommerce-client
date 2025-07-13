35ed740bce2de105f327bf878976d4787eedbb89
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2025-07-13 18:22:20 +0700

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
+  },
+  base: mode === "deploy" ? "/courses-eccommerce-client/" : "/",
+  build: {
+    outDir: "build",
   },
   resolve: {
     alias: {
