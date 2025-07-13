b6bddb5a238f55467f391bb52f323fac11413879
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2025-07-13 18:03:07 +0700

    re-architecture for vite

--- src/data/link.ts
+++ src/data/link.ts
@@ -1,2 +1,1 @@
-// export const LINK_DOMAIN = "https://courses-ecommerce-apis.onrender.com";
-export const LINK_DOMAIN = "https://api.ce.hnam.id.vn";
+export const LINK_DOMAIN = "https://apice.hnam.id.vn/api";
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 // redux, redux toolkit
 import { Provider } from "react-redux";
 // import configureStore from "./reducers/store";
@@ -31,8 +30,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
