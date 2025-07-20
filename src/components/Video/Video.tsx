d586abcb018cf6586f5081eb1566459c88372aa7
nguyentheluan <nguyentheluan.11.12.2000@gmail.com>
2025-07-20 14:37:59 +0700

    remove react-hls-player + react-tuby, change to use hls.js ~ will check later

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef } from "react";
-import ReactHlsPlayer from "react-hls-player/dist";
+import Hls from "hls.js";
 import { useSelector } from "react-redux";
-import { Player } from "react-tuby";
-import "react-tuby/css/main.css";
 import myCourseApi from "src/apis/myCourseApi";
 import { defaultIMG } from "src/assets";
 import { selectAuthorization } from "src/reducers/authSlice";
@@ -12,7 +10,7 @@ interface VideoProps {
   duration?: any;
   courseId?: string;
   lessonId?: string;
-  source?: any;
+  source?: string;
   poster?: string;
 }
 
@@ -22,82 +20,86 @@ const Video: React.FC<VideoProps> = ({
   currentTime,
   source,
   poster,
-  duration,
 }) => {
   const TIME = 1000;
-  // console.log("currentTime", currentTime);
   const { isRole } = useSelector(selectAuthorization);
-
-  const ref = useRef<any>(null);
-  // const [timeline, setTimeline] = useState(0);
-
-  // useEffect(() => {
-  //   ref.current.currentTime = currentTime;
-  // }, [currentTime]);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
-    if (!lessonId || !courseId || isRole !== "student") {
-      return;
+    const video = videoRef.current;
+    if (!video || !source) return;
+
+    if (Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(source);
+      hls.attachMedia(video);
+
+      return () => {
+        hls.destroy();
+      };
+    } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+      video.src = source;
     }
+  }, [source]);
+
+  useEffect(() => {
+    const video = videoRef.current;
+    if (!lessonId || !courseId || isRole !== "student" || !video) return;
 
     let timeline = 0;
-    ref.current?.addEventListener("timeupdate", () => {
-      timeline = ref.current?.currentTime;
-    });
+
+    const updateHandler = () => {
+      timeline = video.currentTime;
+    };
+
+    video.addEventListener("timeupdate", updateHandler);
+
     const interval = setInterval(() => {
       const data = { lessonId, timeline };
-      // console.log("đã lấy được time là", data);
-
       uploadTimeVideo(data);
     }, 5 * TIME);
 
     return () => {
+      video.removeEventListener("timeupdate", updateHandler);
       clearInterval(interval);
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lessonId, courseId]);
+  }, [lessonId, courseId, isRole]);
 
-  const uploadTimeVideo = async (data?: any) => {
+  const uploadTimeVideo = async (data: any) => {
     if (!courseId) return;
-    // console.log("courseId", courseId);
-    // console.log("đã lấy được time là", data);
     try {
       await myCourseApi.updateTimeLineVideoCourse(courseId, data);
-      // console.log(response);
     } catch (error) {
       console.log("lỗi rồi");
     }
   };
 
   return (
-    <Player
-      src={source}
-      // dimensions={{ width: "100%", height: "100%" }}
-      subtitles={[
-        {
-          lang: "en",
-          language: "English",
-          url: "https://cdn.jsdelivr.net/gh/naptestdev/video-examples@master/en.vtt",
-        },
-        {
-          lang: "fr",
-          language: "French",
-          url: "https://cdn.jsdelivr.net/gh/naptestdev/video-examples@master/fr.vtt",
-        },
-      ]}
+    <video
+      ref={videoRef}
       poster={poster || defaultIMG}
-      playerRef={ref}
-      keyboardShortcut={{
-        pause: false,
-        forward: false,
-        rewind: false,
-        fullScreen: false,
-        mute: false,
-        subtitle: false,
+      controls
+      style={{
+        width: "100%",
+        borderRadius: 8,
+        backgroundColor: "#000",
       }}
     >
-      {(ref, props) => <ReactHlsPlayer playerRef={ref} {...props} />}
-    </Player>
+      <track
+        src="https://cdn.jsdelivr.net/gh/naptestdev/video-examples@master/en.vtt"
+        kind="subtitles"
+        srcLang="en"
+        label="English"
+        default
+      />
+      <track
+        src="https://cdn.jsdelivr.net/gh/naptestdev/video-examples@master/fr.vtt"
+        kind="subtitles"
+        srcLang="fr"
+        label="French"
+      />
+      Your browser does not support the video tag.
+    </video>
   );
 };
 
